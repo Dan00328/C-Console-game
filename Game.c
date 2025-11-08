ac2d396eacffe63759e74b6d66efd12490ef458c
Dan00328 <115934655+Dan00328@users.noreply.github.com>
2025-11-08 18:59:58 +0200

    Enhance game logic with scoring and display updates
    Refactored the game loop and added scoring system.

--- Game.c
+++ Game.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main(){
 
 int height=10;
 int lanes=4;
-char M[lanes][height];
-int pozE, laneP,laneE;laneE= rand()%4;laneP=3;pozE=0;
+int pozE, laneP,laneE;
+laneE= rand()%4;laneP=3;pozE=0;
 int i,j;
-int n=1;
-while(n==1){
+char key;
+int score; score=0;
+while(1){
+        system("cls");
     //Desenam drumul
     for(j=0;j<height;j++){
         for(i=0;i<lanes;i++){
+            printf("|");
            if(laneE==i&&j==pozE)
-                printf("V");
+                printf(" V ");
             else if(laneP==i&&j==height-1)
-                printf("A");
+                printf(" A ");
                     else
-                    printf("*");
+                    printf("   ");
         }
-    printf("\n");n--;
+    printf("|\n");
 
     }
+    for(i=0;i<lanes;i++)
+    printf(" ---");
+     printf("\n");
 
-}
+    pozE++;
 
+    if(laneE==laneP&&pozE==height-1){
+        printf("!GAME OVER!");
+    break;
+    }
+
+    if(pozE>height-1){
+        pozE=0;
+        laneE=rand()%4;
+        score++;
+    }
+
+    if (_kbhit()){
+        key= _getch();
+        if(key=='a'&&laneP>0)
+            laneP-=1;
+        if(key=='d'&&laneP<lanes-1)
+            laneP+=1;
+
+    }
+    printf("SCORE:%d",score);
+    Sleep(100);
 }
+return 0;
