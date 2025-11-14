d9b914b820ee3649f05596ef713490e75b44e79e
Dan00328 <115934655+Dan00328@users.noreply.github.com>
2025-11-15 01:00:04 +0200

    Refactor Game.c for improved structure and readability

--- Game.c
+++ Game.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-
+#include <conio.h> //pt citirea tastei apasate
+#include <windows.h> //pt functia sleep()
 
 int main(){
-
+//marimea hartii:
 int height=10;
 int lanes=4;
-int pozE, laneP,laneE;
-laneE= rand()%4;laneP=3;pozE=0;
+//pozitii NPC:
+int pozE,laneE;
+laneE= rand();pozE=0;
+//coordonate player
+int laneP;
+laneP=3;
+//variabile ajutatoare
 int i,j;
-char key;int speed=100;
+char key;
+//stats:
+int speed=100;
 int score; score=0;
+//game:
 while(1){
+        //curata consola pt un frame nou
         system("cls");
+
     //Desenam drumul
     for(j=0;j<height;j++){
         for(i=0;i<lanes;i++){
@@ -27,23 +37,24 @@ while(1){
     printf("|\n");
 
     }
+
     for(i=0;i<lanes;i++)
     printf(" ---");
      printf("\n");
-
+//inamicul se misca
     pozE++;
-
+//conditie de pierdere
     if(laneE==laneP&&pozE==height-1){
         printf("!GAME OVER!");
     break;
     }
-
+//resetare inamic
     if(pozE>height-1){
         pozE=0;
         laneE=rand()%4;
         score++;
     }
-
+//citire input pt control
     if (_kbhit()){
         key= _getch();
         if(key=='a'&&laneP>0)
@@ -56,10 +67,11 @@ while(1){
             speed-=100;
 
     }
+    //printare stats
     printf("SCORE:%d\n",score);
     printf("SPEED Restriction:-%d",speed);
+    //viteza jocului
     Sleep(speed);
 }
 return 0;
 }
-
